9b9de1eb0e654635cc098c9168cfca541ff481e5
aleksey <aleksey.horobryh2023@gmail.com>
2025-08-21 10:05:07 +0300

    group

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,30 +1,32 @@
 import { test, expect } from "@playwright/test";
 
-test("проверка отображение формы авторизации", async ({ page }) => {
-  await page.goto("https://lk.synaptik.ru/login");
-  await expect(page.getByRole("textbox", { name: "Логин *" })).toBeVisible();
-  await expect(page.getByRole("textbox", { name: "Пароль *" })).toBeVisible();
-  await expect(
-    page.getByRole("link", { name: "Забыли пароль?" })
-  ).toBeVisible();
-  await expect(page.getByRole("button", { name: "Войти" })).toBeVisible();
-});
+test.describe("тесты главной страницы", () => {
+  test("проверка отображение формы авторизации", async ({ page }) => {
+    await page.goto("https://lk.synaptik.ru/login");
+    await expect(page.getByRole("textbox", { name: "Логин *" })).toBeVisible();
+    await expect(page.getByRole("textbox", { name: "Пароль *" })).toBeVisible();
+    await expect(
+      page.getByRole("link", { name: "Забыли пароль?" })
+    ).toBeVisible();
+    await expect(page.getByRole("button", { name: "Войти" })).toBeVisible();
+  });
 
-test("проверка названия элементов формы авторизации", async ({ page }) => {
-  await page.goto("https://lk.synaptik.ru/login");
+  test("проверка названия элементов формы авторизации", async ({ page }) => {
+    await page.goto("https://lk.synaptik.ru/login");
 
-  await expect(page.locator("form")).toContainText("Логин");
-  await expect(page.locator("form")).toContainText("Пароль");
-  await expect(
-    page.getByRole("link", { name: "Забыли пароль?" })
-  ).toBeVisible();
-  await expect(page.getByRole("button")).toContainText("Войти");
-});
+    await expect(page.locator("form")).toContainText("Логин");
+    await expect(page.locator("form")).toContainText("Пароль");
+    await expect(
+      page.getByRole("link", { name: "Забыли пароль?" })
+    ).toBeVisible();
+    await expect(page.getByRole("button")).toContainText("Войти");
+  });
 
-test("проверка атрибута хруф у забыли пароль", async ({ page }) => {
-  await page.goto("https://lk.synaptik.ru/login");
+  test("проверка атрибута хруф у забыли пароль", async ({ page }) => {
+    await page.goto("https://lk.synaptik.ru/login");
 
-  await expect(
-    page.getByRole("link", { name: "Забыли пароль?" })
-  ).toHaveAttribute("href", "https://t.me/maximzemtsov");
+    await expect(
+      page.getByRole("link", { name: "Забыли пароль?" })
+    ).toHaveAttribute("href", "https://t.me/maximzemtsov");
+  });
 });
